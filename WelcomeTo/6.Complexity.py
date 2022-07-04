dea6c38bf95ec2b285fc0b52d72fa3640dd21081
진현주 <97022695+realzu@users.noreply.github.com>
2022-07-04 22:46:51 +0900

    알고리즘의 복잡도

---
+++ WelcomeTo/6.Complexity.py
@@ -0,0 +1,15 @@
+# 파트6. 알고리즘의 복잡도(Complexity of Algorithms)
+
+'''
+* 시간 복잡도 (Time Complexity) : 문제의 크기와 이를 해결하는 데 걸리는 시간간의 관계
+
+* Big-O Notation - 빅오 표기법
+O(n) : 선형 시간 알고리즘
+O(log n) : 로그 시간 알고리즘
+O(log n^2) : 이차 시간 알고리즘
+O(nlogn)
+
+* 공간 복잡도 (Space Complexity) : 문제의 크기와 이를 해결하는 데 필요한 메모리 공간 사이의 관계
+
++) 자세한 내용은 블로그에 기록
+'''

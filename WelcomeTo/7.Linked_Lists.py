07228f848f36815bc5a673f00a30cf833ee617ac
진현주 <97022695+realzu@users.noreply.github.com>
2022-07-06 00:09:02 +0900

    연결 리스트1

--- WelcomeTo/7.Linked_Lists.py
+++ WelcomeTo/7.Linked_Lists.py
@@ -1,4 +1,4 @@
-# 파트7. 연결 리스트(Linked Lists) (1)
+# 파트7. 연결 리스트(Linked Lists) (1) 
 
 class Node:
     def __init__(self, item):
@@ -16,7 +16,7 @@ class LinkedList:
 '''
 인덱스 0을 시작번호로 지정하지 않고 임의로 1부터 시작하는게 편할 경우도 있음 (배열이 아니니까)
 '''
- 
+
 # 특정 원소 참조    --k번째노드찾기
 def getAt(self, pos): #pos번째에 있는 노드 자체를 리턴
     if pos <= 0 or pos > self.nodeCount:
@@ -24,7 +24,7 @@ def getAt(self, pos): #pos번째에 있는 노드 자체를 리턴
     
     i = 1
     curr = self.head    #curr= current
-    while i < pos:  #i가 pos보다 작을 동안 반복
+    while i < pos:  #i가 pos보다 작을 동안 반복함
         curr = curr.next #다음 노드로 연결
         i += 1    
     return curr
